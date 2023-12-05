6a33b21d463946dfc689a1fd278db5cc20668086
Dan Chopskie <dchopskie@gmail.com>
2023-12-05 07:43:01 -0600

    Thought of this better algorithm in my sleep

--- src/bin/05.rs
+++ src/bin/05.rs
@@ -1,3 +1,5 @@
+use std::collections::VecDeque;
+
 fn parse(input: &str) -> (Vec<i64>, Vec<Vec<(i64, i64, i64)>>) {
     let spl = input.split("\n\n").collect::<Vec<&str>>();
     let seed_spl = spl[0].split(": ").collect::<Vec<&str>>();
@@ -38,17 +40,52 @@ pub fn part_one(input: &str) -> Option<i64> {
     Some(lowest)
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
+pub fn part_two(input: &str) -> Option<i64> {
     let (seeds, conversions) = parse(input);
-    let mut lowest = i64::MAX;
+
+    let mut ranges = VecDeque::new();
     for i in 0..(seeds.len() / 2) {
-        println!("Checking {}-{}", seeds[2*i], (seeds[2 * i] + seeds[2*i+1]));
-        for seed in seeds[2 * i]..(seeds[2 * i] + seeds[2 * i + 1]) {
-            lowest = lowest.min(get_location(seed, &conversions));
+        ranges.push_back((seeds[2 * i],seeds[2 * i] + seeds[2 * i + 1] - 1));
+    }
+    for conversion in &conversions {
+        let mut new_ranges = VecDeque::new();
+        while let Some(range) = ranges.pop_front() {
+            let mut found = false;
+            for c in conversion {
+                let conversion_start = c.1;
+                let conversion_end = c.1 + c.2;
+                if range.0 >= conversion_start && range.0 < conversion_end {
+                    found = true;
+                    if range.1 < conversion_end {
+                        new_ranges.push_back((range.0 + c.0 - c.1, range.1 + c.0 - c.1));
+                    } else {
+                        new_ranges.push_back((range.0 + c.0 - c.1, conversion_end - 1 + c.0 - c.1));
+                        ranges.push_back((conversion_end, range.1));
+                    }
+                    break;
+                }
+            }
+            if !found {
+                let mut next_lowest = i64::MAX;
+                for c in &conversions[1] {
+                    let conversion_start = c.1;
+                    if conversion_start > range.0 {
+                        next_lowest = next_lowest.max(conversion_start);
+                    }
+                }
+                if next_lowest == i64::MAX || next_lowest > range.1 {
+                    new_ranges.push_back((range.0, range.1));
+                } else {
+                    // This case was never actually hit by my input
+                    new_ranges.push_back((range.0, next_lowest - 1));
+                    ranges.push_back((next_lowest, range.1));
+                }
+            }
         }
-    }    
-    println!("Got {}", lowest);
-    None
+        ranges = new_ranges;
+    }
+    let lowest = ranges.iter().map(|(v,_)| *v).min().unwrap();
+    Some(lowest)
 }
 
 advent_of_code::main!(5);
@@ -66,6 +103,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", 5));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(46));
     }
 }
