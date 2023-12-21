e93fe4b74e61c6260faf1f211d22772b73293a9f
Dan Chopskie <dchopskie@gmail.com>
2023-12-21 13:29:02 -0600

    Remove broken tests

--- src/bin/21.rs
+++ src/bin/21.rs
@@ -156,19 +156,3 @@ pub fn part_two(input: &str) -> Option<u64> {
 
 advent_of_code::main!(21);
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_part_one() {
-        let result = part_one(&advent_of_code::template::read_file("examples", 21));
-        assert_eq!(result, None);
-    }
-
-    #[test]
-    fn test_part_two() {
-        let result = part_two(&advent_of_code::template::read_file("examples", 21));
-        assert_eq!(result, None);
-    }
-}
